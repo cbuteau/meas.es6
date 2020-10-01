a1f7f0b0a50a35a598f547d3fe5f5d7ae4300120
BUTEAU Christopher <CBU1@dsone.3ds.com>
2020-10-01 15:37:26 -0400

    start of dump

--- src/tracker.js
+++ src/tracker.js
@@ -39,4 +39,51 @@ export class Tracker {
       }
     }
   }
+
+  dump(prefix) {
+    var measures = this._perfPtr.getEntriesByType(MEAS_TYPE);
+    var that = this;
+    var filtered = measures.filter(function(m) {
+      return m.name === that.measName;
+    });
+    var prefixPath = prefix || '';
+    if (this.options && this.options.stats) {
+      var sum = 0;
+      var min = Number.MAX_SAFE_INTEGER;
+      var max = Number.MIN_SAFE_INTEGER;
+      var len = filtered.length;
+      for (var i = 0; i < len; i++) {
+        var cur = filtered[i];
+        var dur = cur.duration;
+        sum += dur;
+        if (dur < min) {
+          min = dur;
+        }
+        if (dur > max) {
+          max = dur;
+        }
+      }
+      var avg = sum / len;
+
+      var variance = 0;
+      for (var j = 0; j < len; j++) {
+        var varcalc = filtered[j];
+        variance += (varcalc.duration - avg) ^ 2;
+      }
+      variance /= len;
+
+      var stddev = Math.sqrt(variance);
+
+      var formatted = 'name=' + this.name + ' cnt=' + len + ' avg=' + avg + ' min=' + min + ' max=' + max + ' σ2=' + variance + ' σ=' + stddev;
+      console.log(formatted);
+    } else {
+      var len2 = filtered.length;
+      if (len2 > 1) {
+        console.warn('there are more than one entry...maybe you should do stats');
+      }
+      var current = filtered[len2 - 1];
+      var formatted2 = 'name=' + this.name + ' value=' + current.duration;
+      console.log(formatted2);
+    }
+  }
 }
