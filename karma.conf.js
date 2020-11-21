7c2684ec91ea52c2ccef65d5555164badb63842e
Chris Buteau <cbuteau@gmail.com>
2020-11-21 11:11:14 -0500

    still not working

--- karma.conf.js
+++ karma.conf.js
@@ -103,7 +103,7 @@ module.exports = function(config) {
     junitReporter: {
       outputDir: 'results', // results will be saved as $outputDir/$browserName.xml
       outputFile: 'test_results', // if included, results will be saved as $outputDir/$browserName/$outputFile
-      suite: '', // suite will become the package name attribute in xml testsuite element
+      suite: 'jasmine', // suite will become the package name attribute in xml testsuite element
       useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
       classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
