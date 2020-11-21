2b678b76a8ea9c0d9bd7265fac22af35174ed1a9
Chris Buteau <cbuteau@gmail.com>
2020-11-21 11:13:01 -0500

    adding extension xml

--- karma.conf.js
+++ karma.conf.js
@@ -102,7 +102,7 @@ module.exports = function(config) {
 
     junitReporter: {
       outputDir: 'results', // results will be saved as $outputDir/$browserName.xml
-      outputFile: 'test_results', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputFile: 'test_results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: 'jasmine', // suite will become the package name attribute in xml testsuite element
       useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
