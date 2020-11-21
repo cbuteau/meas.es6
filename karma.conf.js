a6eba94aadee7279591b026d5b96814621570560
Chris Buteau <cbuteau@gmail.com>
2020-11-21 10:37:02 -0500

    trying to publish test results

--- karma.conf.js
+++ karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
 
 
     // web server port
@@ -98,6 +98,17 @@ module.exports = function(config) {
           '--enable-experimental-web-platform-features' // necessary when using importMap option
         ]
       },
+    },
+
+    junitReporter: {
+      outputDir: 'results', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'test_results', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the <properties> section of the report
+      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
     }
   });
 };
